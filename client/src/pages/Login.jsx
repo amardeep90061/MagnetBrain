cc6cef0e33ca6981246a70903d6f82c85e04ff9a
amardeep90061 <64151050+amardeep90061@users.noreply.github.com>
2025-09-30 12:51:46 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
             email,password
         }; 
         try {
-            const url="https://magnet-brain-la6388ra6-amardeep-prajapatis-projects.vercel.app/auth/login"
+            const url="https://magnet-brain-backend.vercel.app/auth/login"
             const response= await fetch(url,{
                 method:"POST",
                 headers:{
