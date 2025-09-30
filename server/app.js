572ac603c41a72a8ddde32f310f69ef33672ab11
amardeep90061 <64151050+amardeep90061@users.noreply.github.com>
2025-09-30 13:45:02 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ mongoose.connect(process.env.MONGODB_URL)
 
   const corsOptions = {
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true,                       
     optionsSuccessStatus: 204
 };
 
