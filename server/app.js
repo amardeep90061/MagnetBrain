11f6028f7b115094617824ddc33d53b09eae4db9
amardeep90061 <64151050+amardeep90061@users.noreply.github.com>
2025-09-30 13:08:21 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('Connected!'));
 
-  const corsOptions = {
-    origin: ['http://localhost:5173','https://magnet-brain-backend.vercel.app/'] // 2. ONLY allow your frontend's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 3. Allow these methods
-    credentials: true, // If you plan to use cookies/sessions
-    optionsSuccessStatus: 204
-};
+//   const corsOptions = {
+//     origin: ['http://localhost:5173','https://magnet-brain-backend.vercel.app/'] // 2. ONLY allow your frontend's origin
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 3. Allow these methods
+//     credentials: true, // If you plan to use cookies/sessions
+//     optionsSuccessStatus: 204
+// };
 
 app.use(express.json());
 app.use(bodyParser.json());
