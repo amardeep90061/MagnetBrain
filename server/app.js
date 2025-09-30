c258b2be3b3e4cb8e799724b9e193f516798b911
amardeep90061 <64151050+amardeep90061@users.noreply.github.com>
2025-09-30 13:44:02 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import taskRoutes from './routes/task.js';
 import authRoutes from './routes/auth.js';
 import mongoose from 'mongoose';
 import cors from 'cors'; 
-import bodyParser from 'body-parser';
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URL)
   .catch(err => console.error('MongoDB connection error:', err));
 
   const corsOptions = {
-    origin: '*'
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
     optionsSuccessStatus: 204
